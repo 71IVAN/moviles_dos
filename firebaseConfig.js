269c998c89cde3b4a0f6aa8ba166f3654fabd0ed
joha2424 <139502619+DecyTru@users.noreply.github.com>
2023-10-25 07:49:38 -0500

    20-10-23

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBtOy_XoVGZqK3pg1oheGnOgrJyNw2tSgs",
+  authDomain: "loginrn-c3775.firebaseapp.com",
+  databaseURL: "https://loginrn-c3775-default-rtdb.firebaseio.com",
+  projectId: "loginrn-c3775",
+  storageBucket: "loginrn-c3775.appspot.com",
+  messagingSenderId: "643748555723",
+  appId: "1:643748555723:web:912c9299b4d1010fd1244e"
+};
